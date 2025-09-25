ac2870e77eb3241141f3c58843e550a2ce6a45cc
Federico-Gz <federicograziani96@gmail.com>
2025-09-25 14:53:44 +0200

    first commit

---
+++ src/test/java/it/federicoGraziani/esercizio/EsercizioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.federicoGraziani.esercizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
